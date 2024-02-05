a7975c3a1c2842aff98bd40219e93c736c052433
DragonXdroid <raysadik5@gmail.com>
2024-02-05 16:37:42 -0600

    Commit 1.1
    expirmental adjustments

--- src/main/java/com/CodingClub/CodeClubZBot/listeners/EventListener.java
+++ src/main/java/com/CodingClub/CodeClubZBot/listeners/EventListener.java
@@ -6,9 +6,18 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class EventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        String message = event.getMessage().getContentRaw();
+        String message = event.getMessage().getContentRaw().toLowerCase();
         if (message.contains("bot")) {
-            event.getChannel().sendMessage("Did you call for me!").queue();
+            event.getChannel().sendMessage("yes @" + event.getAuthor().getName()).queue();
         }
+
+//        if (message.contains("teach")) {
+//            event.getChannel().sendMessage("Hey , I appreciate your perspective, but our decision to have a " +
+//                    "rule against racism in the server is aimed at creating a welcoming and inclusive community for " +
+//                    "everyone. We believe in fostering positive interactions and discussions. If you have any concerns " +
+//                    "or suggestions, feel free to share them. Let's work together to maintain a respectful environment " +
+//                    "for all members.").queue();
+//        }
     }
+    
 }
